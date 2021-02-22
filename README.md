44f90b7768d1e6ff12c579c3f24d76fb2b945805
Gustavo Vergara <gustavovgarcia.sl@gmail.com>
2021-02-22 12:08:12 -0300

    Create the barebones action to output git log

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# git-log-action
+# Git Log action
+
+Get log using GitHub API.
+
+## Inputs
+
+### `baseRef`
+
+**Required** Point of reference to the start of the log. Default `"World"`.
+
+### `headRef`
+
+**Required** Point of reference to the end of the log. Default `"HEAD"`.
+
+## Outputs
+
+### `log`
+
+The log received from GitHub's API.
+
+## Example usage
+
+uses: GustavoVergara/git-log-action@v1.0.0
+with:
+  baseRef: 'v0.0.5'
---
+++ index.ts
@@ -0,0 +1,58 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { context, GitHub } from '@actions/github/lib/utils';
+
+let _octokit;
+function getOctokit(): InstanceType<typeof GitHub> {
+    if (_octokit != null) {
+        return _octokit;
+    }
+    const token = process.env.GITHUB_TOKEN;
+    if (token == null) {
+        throw {message: "'GITHUB_TOKEN' not set"};
+    }
+    _octokit = github.getOctokit(token);
+    return _octokit;
+}
+
+async function getLatestTag() {
+    const tags = await getOctokit().repos.listTags({
+        ...context.repo,
+        per_page: 100,
+    });
+    
+    // if (tags.data.length == 0) {
+    //     throw {message: "Coundnt fetch tags"}
+    // }
+    return tags.data[0].name;    
+}
+
+async function getCommits() {
+    let baseRef = core.getInput('baseRef');
+    if (baseRef == null) {
+        baseRef = await getLatestTag();
+    }
+    const headRef = core.getInput('headRef');
+
+    const commits = await getOctokit().repos.compareCommits({
+        ...context.repo,
+        base: baseRef,
+        head: headRef,
+    });
+
+    return commits.data.commits;
+}
+
+export default async function main() {
+    try {
+        const commits = await getCommits();
+        const changelog = commits
+            .filter((commit) => !!commit.commit.message)
+            .map((commit) => commit.commit.message)
+            .join("\n");
+        
+        core.setOutput("changelog", changelog)
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
